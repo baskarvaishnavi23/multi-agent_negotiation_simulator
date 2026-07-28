cf358ec3603754567d37590b3054858ecaa38707
Vaishnavi Baskar <vaishnavibaskar190@gmail.com>
2026-07-28 17:24:49 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './components/LoginPage'; // <-- Import the new Login page
 import HomePage from './components/HomePage';
 import NegotiationArena from './components/NegotiationArena';
 import OutcomeReport from './components/OutcomeReport';
@@ -9,7 +10,11 @@ function App() {
     <Router>
       <div className="min-h-screen bg-[#0B0E14] text-gray-200">
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          {/* Set Login as the root path */}
+          <Route path="/" element={<LoginPage />} />
+          
+          {/* Existing Routes */}
+          <Route path="/home" element={<HomePage />} />
           <Route path="/arena" element={<NegotiationArena />} />
           <Route path="/report" element={<OutcomeReport />} />
         </Routes>
