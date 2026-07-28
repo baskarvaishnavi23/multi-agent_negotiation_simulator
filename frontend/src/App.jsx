49f4e65192a57f0fc302e644331505d92b7ad42d
Vaishnavi Baskar <vaishnavibaskar190@gmail.com>
2026-07-28 18:55:07 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from './components/LoginPage'; // <-- Import the new Login page
+import LoginPage from './components/LoginPage';
 import HomePage from './components/HomePage';
 import NegotiationArena from './components/NegotiationArena';
 import OutcomeReport from './components/OutcomeReport';
@@ -10,11 +10,17 @@ function App() {
     <Router>
       <div className="min-h-screen bg-[#0B0E14] text-gray-200">
         <Routes>
-          {/* Set Login as the root path */}
+          {/* 
+            Option 1: Login is the first page you see.
+            If you want Home to be root, swap "/" and "/login" below.
+          */}
           <Route path="/" element={<LoginPage />} />
+          <Route path="/login" element={<LoginPage />} />
           
-          {/* Existing Routes */}
+          {/* Dashboard / HomePage */}
           <Route path="/home" element={<HomePage />} />
+          
+          {/* Existing Internal Routes */}
           <Route path="/arena" element={<NegotiationArena />} />
           <Route path="/report" element={<OutcomeReport />} />
         </Routes>
